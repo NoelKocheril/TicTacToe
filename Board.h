b0e06d205dd76724d632454a529dc906877a2385
NoelKocheril <NoelKocheril@hotmail.com>
2018-03-12 11:29:03 -0400

    Cell.cpp & .h

--- Board.h
+++ Board.h
@@ -1,13 +1,28 @@
 //
 // Created by noelk on 3/12/2018.
 //
+#include <string>
+#include <iostream>
 
 #ifndef TICTACTOE_BOARD_H
 #define TICTACTOE_BOARD_H
 
+using namespace std;
 
 class Board {
+    bool grid[9];
+    Board() {
+     for (int i = 0; i < 9; i++){
+         grid[i] = false;
+     }
+    }
+    string toString(){
+        printf("   |   |   ");
+        printf(" %d | %d | %d ");
+        printf("   |   |   ");
+        printf("___________");
 
+    }
 };
 
 
---
+++ Cell.h
@@ -0,0 +1,21 @@
+//
+// Created by noelk on 3/12/2018.
+//
+
+#ifndef TICTACTOE_CELL_H
+#define TICTACTOE_CELL_H
+
+#include <stdlib.h>
+#include <cstdio>
+#include <string>
+
+class Cell {
+    bool filled = false;
+    char player = NULL;
+    Cell(bool filled, char player) {
+
+    }
+};
+
+
+#endif //TICTACTOE_CELL_H
